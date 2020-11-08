d90357bab3a6f5d09e9e41e757f1c1e64d9fdee6
cschrist <cschrist@LAB-060>
2020-11-08 15:44:33 -0600

    Created SQL connection string Factory.

---
+++ DataAccess/Factories/SqlConnectionStringFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace WetEntryKayak.DatabaseAccess.Factories
+{
+    internal static class SqlConnectionStringFactory
+    {
+        internal static string Execute()
+        {
+            SqlConnectionStringBuilder stringbuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = "essql1.walton.uark.edu",
+                UserID = "PROJECTF2027",
+                Password = "SH93ack$",
+                InitialCatalog = "PROJECTF2027"
+            };
+            return stringbuilder.ConnectionString;
+        }
+    }
+}
